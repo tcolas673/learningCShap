e9ea3e7bd8035bfc951dbe93605b574e8d6e90f0
tcolas673 <tcolas673@gmail.com>
2020-02-10 10:50:25 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c1 = new MyClass();
+            Console.WriteLine($"Hello World! {c1.ReturnMessage()}");
+            Console.WriteLine(Math.Pow(3,2));
+            Console.Write("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name!);
+        }
+    }
+}
